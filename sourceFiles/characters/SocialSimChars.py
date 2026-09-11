dc81d3ab11b97187e90ce45d4040b3b5b2c9a49c
R Ochoa <167141768+Ochoa-R@users.noreply.github.com>
2026-09-10 19:22:39 -0700

    Refactored Inventory and Player
    - Separated the inventory into its own class to give it its own interfacing methods
    - Test code has been expanded to verify the functionality of all new features

--- sourceFiles/characters/SocialSimChars.py
+++ sourceFiles/characters/SocialSimChars.py
@@ -2,7 +2,7 @@
 # Antonio Ochoa, Joseph Endozo
 
 class Character:
-    """Parent class designed to hold the name of a character
+    """Parent class designed to hold the traits of a character
     in the game"""
     def __init__(
         self, 
@@ -15,6 +15,39 @@ class Character:
     ):
         self.traits = traits
 
+
+class Inventory:
+    """Class for managing the player's inventory, with functions for adding and
+        removing items, opening and closing, and returning whether the
+        inventory is opened or closed"""
+    def __init__(self, inventory = [], invState = False):
+        self.__inventory = inventory
+        self.__invState = invState
+
+    def isOpen(self):
+        return self.__invState
+    def open(self):
+        if len(self.__inventory) == 0:
+            print("Inventory empty!\n")
+        else:
+            count = 1
+            for item in self.__inventory:
+                print(f"{count}) {self.readItemName(item)}")
+                count += 1
+            self.__invState = True
+    def close(self):
+        self.__invState = False 
+    def addItem(self, item):
+        self.__inventory.append(item)
+    def readItemName(self, item):
+        return item["Name"]
+    def readItemDesc(self, selectedItem):
+        for item in self.__inventory:
+            if item["Name"] == selectedItem:
+                    return item["Description"]
+        else:
+            return "You don't have that!"
+
 class Player(Character):
     """Child class of parent, represents the user's player
     character. Contains an inventory that the player can hold
@@ -22,28 +55,23 @@ class Player(Character):
     def __init__(
         self, 
         playerTraits, 
-        inventory=["Apple"], 
+        inventory=Inventory(), 
         energy = 0
     ):
         super().__init__(playerTraits)
         self.inventory = inventory
         self.energy = energy
-    def openInventory(self):
-        if len(self.inventory) == 0:
-            print("Inventory empty!\n")
-        else:
-            count = 1
-            for item in self.inventory:
-                print(count,")",item)
-                count += 1
+    '''
     def giveItem(self):
         response = input("Which item would you like to gift?: ")
         if response in self.inventory:
             self.inventory.pop(response)
-            return response
+            return respons
+            e
         else:
             print("You don't have that!")
-'''
+    '''
+
 class NPC(Character):
     """Child class of parent, represents a non-player
     character that the player can interact with. Contains
@@ -53,6 +81,7 @@ class NPC(Character):
         super().__init__(npcName, attachment=0)
         self.preferences = preferences
         self.attachment = 0
+    '''
     def receiveGift(self, gift):
         if gift in self.preferences["likes"]:
             print(f"{self.name} likes your gift!")
@@ -62,22 +91,55 @@ class NPC(Character):
             self.attachment -= 10
         else:
             pirint(f"{self.name} appreciated the thought.")
-'''       
-        
+    '''       
 
 def getPlayerInfo():
     infoHold = Character()
-    for key in infoHold.traits.keys():
-        attribute = ""
-        confirm = ""
-        while confirm != "Yes" and confirm != "yes":
-            attribute = input(f"What's your {key}?: ")
-            print(f"Your {key} is:", attribute)
-            confirm = input("is this okay?: ")
-        infoHold.traits[key] = attribute
+    confirm = ""
+    while confirm != "Yes" and confirm != "yes":
+        for key in infoHold.traits.keys():
+            attribute = input(f"What's your {key}?:")
+            infoHold.traits[key] = attribute
+        for key, value in infoHold.traits.items():
+            print(f"Your {key} is: {value}")
+        confirm = input("is this okay?: ")
     return infoHold
 
-mainCharacter = Player(getPlayerInfo())
-# print(len(mainCharacter.inventory))
-mainCharacter.openInventory()
-leave = input("Press any key to leave: ")
+def checkPlayerStats():
+    chungs = 5
+
+def main():
+    
+    mainCharacter = Player(getPlayerInfo())
+    # print(len(mainCharacter.inventory))
+    mainCharacter.inventory.open()
+
+    giftItem = {
+        "Name": "Apple",
+        "Description": "A succulent, yet healthy snack. A favorite of those who take their nutrition seriously!"
+    }
+
+    giftItemTwo = {
+        "Name": "Masamune",
+        "Description": "A legendary holy sword used to fell a dark mage in 600 A.D. Smells a bit like a swamp..."
+    }
+    mainCharacter.inventory.addItem(giftItem)
+    mainCharacter.inventory.addItem(giftItemTwo)
+    
+    mainCharacter.inventory.open()
+    while(mainCharacter.inventory.isOpen()):
+        try:
+            action = int(input(
+                "What would you like to do?\n1) close\n2) read item description\n(type the number of the action you want to perform):"))
+            if action == 1:
+                mainCharacter.inventory.close()
+            elif action == 2:
+                selectedItem = input("Which item would you like to see the description of?\n(type the name of the item):")
+                print(mainCharacter.inventory.readItemDesc(selectedItem))
+            else:
+                print("That's not an option!")
+        except ValueError:
+             print("That's not an option!")
+
+main()
+
