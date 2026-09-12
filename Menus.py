abab8befca7bb7384ce3d0bf4fcd63751d9bb1c1
Joseph E <deathreekd@gmail.com>
2026-09-11 21:27:17 -0700

    Update Menus.py

--- Menus.py
+++ Menus.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import json
 now = datetime.now()
+id_list = []
 
 def startGame():
     askUserAfterStart = input("New Game | Load Save | Achievements | Exit")
@@ -15,10 +16,30 @@ def startGame():
     else
         print("Invalid Function")
 
+saves = {
+    0: {
+        "achievement1": {
+            "haveAchievement": True, 
+            "timeComplete": "hello", 
+            "dateComplete": "your mom"},
+        "achievement2": {
+            "haveAchievement": False, 
+            "timeComplete": 0, 
+            "dateComplete": 0
+        }
+    }
+}
 #achievement menu below
 achievementDict = {
-    "achievement1": {"haveAchievement": True, "timeComplete": 0, "dateComplete": 0},
-    "achievement2": {"haveAchievement": False, "timeComplete": 0, "dateComplete": 0}
+    "achievement1": {
+        "haveAchievement": True, 
+        "timeComplete": "hello", 
+        "dateComplete": "your mom"},
+    "achievement2": {
+        "haveAchievement": False, 
+        "timeComplete": 0, 
+        "dateComplete": 0
+    }
 }
 
 def achievementGet(refAchiev):
@@ -31,21 +52,46 @@ def achievementGet(refAchiev):
 
 #Creating Saves
 def saveGame():
-    playerResponse = input("Would you like to create a new save file?    Type Yes or No")
-    if playerResponse == "Yes":
-        
-    elif playerResponse == "No":
-
+    def saveGame():
+    filename = "save"
+    #Check if user has save file already
+    if fileExistsCheck() == False:
+        #Creates new save file
+        id_list.append(0)
+        with open(filename, 'w') as f:
+            json.dump(saves, f)
+    #Creates a new save depending on input
+    elif fileExistsCheck() == True:
+        #Creates a new save
+        newSaveResponse = input("Would you like to create a new save?    Type Yes or No")
+        if playerResponse == "Yes":
+            #Appends the new save ID to a list then saves the achievement dictionary to that ID
+            id_list.append(len(id_list))
+            saves[len(id_list) - 1] = achievementDict
+            with open(filename, 'w') as f:
+                json.dump(saves, f)
+        elif playerResponse == "No":
+            #Replaces the specified save the user chooses
+            print("Please type the number of save you want to replace")
+            for number in id_list:
+                print("Save ", number)
+            replaceSaveResponse = int(input())
+            saves[replaceSaveResponse] = achievementDict
+            with open(filename, 'w') as f:
+                json.dump(saves,f)
     else:
-        print("That wasn't an option, please try again.")
-    with open(save, 'w') as f:
-        json.dump(achievementDict, f)
-        print("Game Saved")
+        print("Something went wrong here.... Please try again")
 
 #Loading saves
 def loadSave():
-    
-        
+    filename = 'saves_file'
+    with open(filename) as f:
+        saves = json.load(f)
+    print("Please choose which save you would like to load")
+    for number in id_list
+        print(id_list[number])
+    loadSaveResponse = input()
+    achievementDict = saves[loadSaveResponse]
                            
 
 def newGame():
